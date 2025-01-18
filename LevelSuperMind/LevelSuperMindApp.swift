b8cc629d2e10735cea62cc366b131364470b264c
aaseem22 <aaseem.mhaskar.am@gmail.com>
2025-01-18 20:17:42 +0530

    Initial Commit

---
+++ LevelSuperMind/LevelSuperMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  LevelSuperMindApp.swift
+//  LevelSuperMind
+//
+//  Created by Aaseem Mhaskar on 18/01/25.
+//
+
+import SwiftUI
+
+@main
+struct LevelSuperMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
